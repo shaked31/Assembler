cb60511a76b4e2415f607f1518e3ed3fecaf0d04
Shaked Pollak <shaked.pollak05@gmail.com>
2026-07-27 23:25:52 +0300

    finished first pass

--- include/first_pass.h
+++ include/first_pass.h
@@ -1,6 +1,18 @@
 #ifndef FIRST_PASS_H
 #define FIRST_PASS_H
 
-int run_first_pass(const char* filename);
+#include "globals.h"
+
+/**
+ * @fn run_first_pass
+ * @brief Executes the first pass of the assembly process.
+ *        This function reads the .am file line by line.
+ *        It decides whether each line is a directive or code instruction
+ *        It builds the inital symbol table (without .entry) and calculates the memory needed for code (IC) and data (DC)
+ * @param[in]  filename  The full name of the .am file to go through
+ * @param[out] sym_head  Pointer to the head of the symbol table linked list
+ * @return               An integer of status based on status_t enum
+ */
+int run_first_pass(const char* filename, symbol_node_t **sym_head);
 
 #endif
--- src/first_pass.c
+++ src/first_pass.c
@@ -3,31 +3,95 @@
 #include "../include/instructions.h"
 #include "../include/symbol_table.h"
 #include "../include/globals.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define EXTERN_DIRECTIVE ".extern"
-#define ENTRY_DIRECTIVE ".entry"
-#define DB_DIRECTIVE ".db"
-#define DH_DIRECTIVE ".dh"
-#define DW_DIRECTIVE ".dw"
-#define ASCIZ_DIRECTIVE ".asciz"
+#define EXTERN_DIRECTIVE (".extern")
+#define ENTRY_DIRECTIVE (".entry")
+#define DB_DIRECTIVE (".db")
+#define DH_DIRECTIVE (".dh")
+#define DW_DIRECTIVE (".dw")
+#define ASCIZ_DIRECTIVE (".asciz")
 
+#define DATA_BYTE (1)
+#define DATA_HALF_WORD (2)
+#define DATA_WORD (4)
+
+/**
+ * @fn handle_directive
+ * @brief The function processes a data or structural directive
+ *        It handles directives like .extern, .entry, .db, .dh, .dw, and .asciz
+ *        It registers labels into the symbol table linked list
+ *        It increments the Data Counter (DC) according to the memory required
+ * 
+ * @param[in]     parsed    Pointer to the parsed line struct containing the lines details
+ * @param[in,out] sym_head  Pointer to the head of the symbol table linked list
+ * @param[in,out] DC        Pointer to a integer of the current Data Counter
+ * @return                  An integer of status based on status_t enum
+ */
 static int handle_directive(parsed_line_t *parsed, symbol_node_t **sym_head, int *DC);
+
+/**
+ * @fn handle_code
+ * @brief The function processes an assembly instruction
+ *        It checks that the instruction exists
+ *        It registers labels into the symbol table linked list as a code symbol
+ *        It increments the Instruction Counter (IC) according to the memory required
+ * 
+ * @param[in]     parsed    Pointer to the parsed line struct containing the lines details
+ * @param[in,out] sym_head  Pointer to the head of the symbol table linked list
+ * @param[in,out] IC        Pointer to a integer of the current Instruction Counter
+ * @return                  An integer of status based on status_t enum
+ */
 static int handle_code(parsed_line_t *parsed, symbol_node_t **sym_head, int *IC);
+
+/**
+ * @fn update_data_symbols
+ * @brief The function offsets the memory addresses of all data symbols
+ * The machine stores data memory immediately after code memory
+ * This function adds the final Instruction Counter (IC) value to the address of every symbol 
+ * marked with the 'is_data' flag.
+ * 
+ * @param[in,out] sym_head  Pointer to the head of the symbol table linked list
+ * @param[in]     final_IC  The final calculated Instruction Counter value
+ * @return                  An integer of status based on status_t enum
+ */
 static int update_data_symbols(symbol_node_t **sym_head, int final_IC);
+
+/**
+ * @fn count_data_operands
+ * @brief The function counts the number of numeric tokens in a comma-separated operand string.
+ * It calculate how many individual numbers are present for .db, .dh, or .dw directives.
+ * 
+ * @param[in] operands  The raw string containing the operands.
+ * @return              An integer representing the number of numeric tokens found.
+ */
+static int count_data_operands(const char* operands);
 
 int run_first_pass(const char* filename, symbol_node_t **sym_head) {
     FILE *am_fptr = NULL;
     char line_buffer[MAX_LINE_LEN];
+    char *am_filename = NULL;
     parsed_line_t parsed_line;
     status_t status = STATUS_UNINITIALIZED;
 
     int IC  = IC_START_ADDR;
     int DC  = DC_START_ADDR;
 
-    am_fptr = fopen(filename, "r");
+    am_filename = (char*)malloc(strlen(filename) + FILE_EXTENTION_SIZE);
+
+    if (am_filename == NULL) {
+        fprintf(stderr, "Couldn't allocate memory for .as file extention\n");
+        status = STATUS_FAILURE_MEMORY_ALLOCATION;
+        goto lb_cleanup;
+    }
+
+    sprintf(am_filename, "%s.am", filename);
+
+    am_fptr = fopen(am_filename, "r");
     if (am_fptr == NULL) {
         fprintf(stderr, "Couldn't open file %s in write mode\n", filename);
         status = STATUS_FAILURE_FILE_MGMT;
@@ -42,10 +106,14 @@ int run_first_pass(const char* filename, symbol_node_t **sym_head) {
         }
 
         if (parsed_line.operation[0] == '.') {
-            status = (int)handle_directive(&parsed_line, &DC, sym_head);
+            status = (int)handle_directive(&parsed_line, sym_head, &DC);
         }
         else {
-            status = (int)handle_code(&parsed_line, &IC, sym_head);
+            status = (int)handle_code(&parsed_line, sym_head, &IC);
+        }
+
+        if (status != STATUS_SUCCESS) {
+            goto lb_cleanup;
         }
     }
 
@@ -54,21 +122,98 @@ int run_first_pass(const char* filename, symbol_node_t **sym_head) {
     status = STATUS_SUCCESS;
 
 lb_cleanup:
+FREE_VAR(am_filename);
 CLOSE_FILE(am_fptr);
 return (int)status;
 }
 
 static int handle_directive(parsed_line_t *parsed, symbol_node_t **sym_head, int *DC) {
-    symbol_node_t *existing_sym;
+    symbol_node_t *existing_sym = NULL;
     int num_elements = 0;
     int data_size_multiplier = 0;
     char *str_start, *str_end;
+    status_t status = STATUS_UNINITIALIZED;
 
-    if (strcmp(parsed->operation, EXTERN_DIRECTIVE) == 0) {}
+    if (strcmp(parsed->operation, EXTERN_DIRECTIVE) == 0) {
+        /* Register a .extern directive */
+        existing_sym = find_symbol(*sym_head, parsed->operands);
+        if (existing_sym != NULL) {
+            fprintf(stderr, "External symbol '%s' is already defined\n", parsed->operands);
+            status = STATUS_FAILURE_DUPLICATE_EXTERNAL_SYM;
+            goto lb_cleanup;
+        }
+
+        status = insert_symbol(sym_head, parsed->operands, 0, 0, 0, 0, 1);
+        if (status != STATUS_SUCCESS)
+            goto lb_cleanup;
+    }
+    
+    else if (strcmp(parsed->operation, ENTRY_DIRECTIVE) == 0) {
+        /* Skip entry directives in the first pass
+           Because the label is defined later in the file, and in the first pass, we don't know yet where it exists in the file */
+        status = STATUS_SUCCESS;
+        goto lb_cleanup;
+    }
+
+    else if (parsed->label[0] != '\0') {
+        /* Register a label for data directives */
+        existing_sym = find_symbol(*sym_head, parsed->label);
+        if (existing_sym != NULL) {
+            fprintf(stderr, "Label '%s' is already defined\n", parsed->label);
+            status = STATUS_FAILURE_DUPLICATE_LABEL_DEF;
+            goto lb_cleanup;
+        }
+
+        status = insert_symbol(sym_head, parsed->label, *DC, 0, 1, 0, 0);
+        if (status != STATUS_SUCCESS)
+            goto lb_cleanup;
+    }
+
+    else if (strcmp(parsed->operation, ASCIZ_DIRECTIVE) == 0) {
+        str_start = strchr(parsed->operands, '"');
+        if (str_start == NULL) {
+            fprintf(stderr, "Invalid string format in .asciz\n");
+            status = STATUS_FAILURE_INVALID_STR_IN_ASCIZ;
+            goto lb_cleanup;
+        }
+        str_end = strrchr(str_start + 1, '"');
+        if (str_end == NULL) {
+            fprintf(stderr, "Invalid string format in .asciz\n");
+            status = STATUS_FAILURE_INVALID_STR_IN_ASCIZ;
+            goto lb_cleanup;
+        }
+        *DC += (str_end - str_start);
+        status = STATUS_SUCCESS;
+        goto lb_cleanup;
+    }
+
+    else if (strcmp(parsed->operation, DB_DIRECTIVE) == 0)
+        data_size_multiplier = DATA_BYTE;
+    else if (strcmp(parsed->operation, DH_DIRECTIVE) == 0)
+        data_size_multiplier = DATA_HALF_WORD;
+    else if (strcmp(parsed->operation, DW_DIRECTIVE) == 0)
+        data_size_multiplier = DATA_WORD;
+
+    else {
+        /* Unknown directive */
+        fprintf(stderr, "Unknown directive '%s'\n", parsed->operation);
+        status = STATUS_FAILURE_UNKNOWN_OPERATION;
+        goto lb_cleanup;
+    }
+
+    if (data_size_multiplier > 0) {
+        num_elements = count_data_operands(parsed->operands);
+        *DC += (num_elements * data_size_multiplier);
+    }
+
+    status = STATUS_SUCCESS;
+
+lb_cleanup:
+return (int)status;
 }
 
 static int handle_code(parsed_line_t *parsed, symbol_node_t **sym_head, int *IC) {
-    instruction_info_t *instruction_info = NULL;
+    const instruction_info_t *instruction_info = NULL;
     symbol_node_t *existing_sym = NULL;
     status_t status = STATUS_UNINITIALIZED;
 
@@ -101,5 +246,48 @@ return (int)status;
 }
 
 static int update_data_symbols(symbol_node_t **sym_head, int final_IC) {
+    status_t status = STATUS_UNINITIALIZED;
+    symbol_node_t *curr = *sym_head;
+    
+    if (curr == NULL) {
+        /* Symbols table is empty, nothing to update */
+        status = STATUS_SUCCESS;
+        goto lb_cleanup;
+    }
 
+    while (curr != NULL) {
+        /* If the symbol is a data directive (.db, .dh, .dw, .asciz) */
+        if (curr->is_data == 1) {
+            curr->address += final_IC;
+        }
+        curr = curr->next;
+    }
+
+    status = STATUS_SUCCESS;
+    
+lb_cleanup:
+return (int)status;
+}
+
+static int count_data_operands(const char* operands) {
+    int count = 0;
+    unsigned char in_token = 0;
+    const char *ptr = operands;
+
+    while (*ptr != '\0') {
+        /* If current char is space or comma, we are not in a number */
+        if (*ptr == ',' || isspace(*ptr)) {
+            in_token = 0;
+        }
+
+        /* Only if entering now to a new number, increase the count
+           Else, skip */
+        else if (!in_token) {
+            in_token = 1;
+            count++;
+        }
+        ptr++;
+    }
+    
+    return count;
 }
